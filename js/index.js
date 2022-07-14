6c7cd9df92e597a3c214c3242929b81365f8b230
mettamegan <99057865+mettamegan@users.noreply.github.com>
2022-07-14 14:04:15 -0500

    Updates from Tutoring Sessions
    Adds to coded Portfolio from tutor sessions on 07.14.22, 07.07.22, & 6.30.22

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// Add Event Listener Below
+function down1() {
+    document.getElementById("caseStudies").scrollIntoView();
+  }
+  function down1redirect() {
+    document.getElementById("caseStudies").scrollIntoView();
+  }
+  function down2() {
+    document.getElementById("playground").scrollIntoView();
+  }
+  function down2redirect() {
+    document.getElementById("playground").scrollIntoView();
+  }
+  function down3() {
+    document.getElementById("Resume").scrollIntoView();
+  }
+  function down3redirect() {
+    document.getElementById("Resume").scrollIntoView();
+  }
+  function down4() {
+    document.getElementById("About").scrollIntoView();
+  }
+  function down4redirect() {
+    document.getElementById("About").scrollIntoView();
+  }
+  function down5() {
+    document.getElementById("Contact").scrollIntoView();
+  }
+  function down5redirect() {
+    document.getElementById("Contact").scrollIntoView();
+  }
